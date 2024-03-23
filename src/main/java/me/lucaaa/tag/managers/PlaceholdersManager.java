eed06005b2b17190557bcef14b59fbfda994c52e
Luncaaa <luca.alavoir@gmail.com>
2024-03-23 18:07:09 +0100

    [1.1] - Update plugin version and dependencies

--- src/main/java/me/lucaaa/tag/managers/PlaceholdersManager.java
+++ src/main/java/me/lucaaa/tag/managers/PlaceholdersManager.java
@@ -23,7 +23,7 @@ public class PlaceholdersManager extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.0";
+        return "1.1";
     }
 
     @Override
