ee1a9d2c9d060548c32f10d139946e42895dde2c
Luncaaa <luca.alavoir@gmail.com>
2024-07-28 02:17:54 +0200

    [1.3] - Fix deprecation in newer Java versions

--- src/main/java/me/lucaaa/tag/managers/UpdateManager.java
+++ src/main/java/me/lucaaa/tag/managers/UpdateManager.java
@@ -5,7 +5,8 @@ import org.bukkit.Bukkit;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
@@ -20,12 +21,12 @@ public class UpdateManager {
     public void getVersion(final Consumer<String> consumer) {
         Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
             try {
-                InputStream resourcePage = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.RESOURCE_ID + "/~").openStream();
+                InputStream resourcePage = new URI("https://api.spigotmc.org/legacy/update.php?resource=" + this.RESOURCE_ID + "/~").toURL().openStream();
                 Scanner scanner = new Scanner(resourcePage);
                 if (scanner.hasNext()) {
                     consumer.accept(scanner.next());
                 }
-            } catch (IOException e) {
+            } catch (IOException | URISyntaxException e) {
                 plugin.getLogger().info("Unable to check for updates: " + e.getMessage());
             }
         });
